51369d6c7f4f2045b21872ee59979396c69f0ba4
Justin C Presley <justincpresley@gmail.com>
2021-03-02 02:48:12 -0600

    More Tests

--- tests/test_state_vector.py
+++ tests/test_state_vector.py
@@ -3,7 +3,7 @@ import pytest
 sys.path.insert(0,'.')
 from svs.state_vector import StateVector
 
-def state_vector_ordering():
+def test_state_vector_ordering():
     sv1 = StateVector()
     sv1.set("c", 5)
     sv1.set("a", 6)
@@ -18,9 +18,10 @@ def state_vector_ordering():
     sv2.set("B", 10)
     sv2.set("c", 5)
 
-    return sv1.to_str() == sv2.to_str()
+    assert sv1.to_str() == sv2.to_str()
+    assert sv1.encode() == sv2.encode()
 
-def state_vector_component():
+def test_state_vector_component():
     sv = StateVector()
     sv.set("c", 5)
     sv.set("a", 6)
@@ -28,10 +29,30 @@ def state_vector_component():
     sv.set("z", 15)
     sv.set("x", 5225)
 
-    return StateVector(sv.to_component()).encode() == sv.encode()
+    svc = StateVector(sv.to_component())
 
-def test_state_vector_ordering():
-    assert state_vector_ordering() == True
+    assert sv.to_str() == svc.to_str()
+    assert sv.encode() == svc.encode()
 
-def test_state_vector_component():
-    assert state_vector_component() == True
+def test_state_vector_get():
+    sv = StateVector()
+    sv.set("b", 5)
+    sv.set("a", 6)
+
+    assert sv.get("a") == 6
+    assert sv.get("b") == 5
+
+def test_state_vector_has():
+    sv = StateVector()
+    sv.set("b", 5)
+    sv.set("a", 6)
+
+    assert sv.has("a")
+    assert sv.has("b")
+
+def test_state_vector_set():
+    sv = StateVector()
+    sv.set("a", 10)
+    sv.set("a", 100)
+
+    assert sv.get("a") == 100
