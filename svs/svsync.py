8b75f35ad05fced6c56521f893d5a7e20781acb9
Justin C Presley <justincpresley@gmail.com>
2021-03-06 00:07:05 -0600

    Shared Updated

--- svs/svsync.py
+++ svs/svsync.py
@@ -12,5 +12,42 @@ class SVSync(SVSyncBase):
         super().__init__(app, groupPrefix, groupPrefix, nid+groupPrefix, nid, updateCallback, storage)
 
 class SVSyncShared(SVSyncBase):
-    def __init__(self, app:NDNApp, groupPrefix:Name, nid:Name, updateCallback:Callable, storage:Optional[SVSyncStorageBase]=None) -> None:
-        super().__init__(app, groupPrefix, groupPrefix+[Component.from_str("s")], groupPrefix+[Component.from_str("d")], nid, updateCallback, storage)
+    def __init__(self, app:NDNApp, groupPrefix:Name, nid:Name, updateCallback:Callable, cacheOthers:bool, storage:Optional[SVSyncStorageBase]=None) -> None:
+        self.cacheOthers = cacheOthers
+        preDataPrefix = groupPrefix + [Component.from_str("d")]
+        if not self.cacheOthers:
+            preDataPrefix = preDataPrefix + nid
+        super().__init__(app, groupPrefix, groupPrefix+[Component.from_str("s")], preDataPrefix, nid, updateCallback, storage)
+    async def fetchData(self, nid:Name, seqNum:int, retries:int=0) -> Optional[bytes]:
+        name = self.dataPrefix + nid + Name.from_str( "/epoch-"+str(seqNum) )
+        while retries+1 > 0:
+            try:
+                logging.info(f'SVS_Socket: fetching data {Name.to_str(name)}')
+                ex_int_name, meta_info, content = await self.app.express_interest(name, must_be_fresh=True, can_be_prefix=False, lifetime=6000)
+                logging.info(f'SVS_Socket: received data {bytes(content)}')
+                if content:
+                    data_packet = make_data(name, MetaInfo(freshness_period=5000), content=bytes(content))
+                    logging.info(f'SVS_Socket: publishing others data {Name.to_str(name)}')
+                    self.storage.put_data_packet(name, data_packet)
+                return bytes(content) if content else None
+            except InterestNack as e:
+                logging.warning(f'SVS_Socket: nacked with reason={e.reason}')
+            except InterestTimeout:
+                logging.warning(f'SVS_Socket: timeout')
+            except InterestCanceled:
+                logging.warning(f'SVS_Socket: canceled')
+            except ValidationFailure:
+                logging.warning(f'SVS_Socket: data failed to validate')
+            except Exception as e:
+                logging.warning(f'SVS_Socket: unknown error has occured: {e}')
+
+            retries = retries - 1
+            if retries+1 > 0:
+                logging.warning(f'SVS_Socket: retrying fetching data')
+        return None
+    def publishData(self, data:bytes) -> None:
+        name = self.dataPrefix + self.nid + Name.from_str( "/epoch-"+str(self.logic.getCurrentSeqNum()+1) )
+        data_packet = make_data(name, MetaInfo(freshness_period=5000), content=data)
+        logging.info(f'SVS_Socket: publishing self data {Name.to_str(name)}')
+        self.storage.put_data_packet(name, data_packet)
+        self.logic.updateState()
