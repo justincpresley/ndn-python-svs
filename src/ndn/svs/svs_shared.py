6f66f69e7bfa72a2cc110b85f62bb1d9503ccf75
Justin C Presley <justincpresley@gmail.com>
2021-03-25 20:04:21 -0500

    Get Raw Packet when fetching Data

--- src/ndn/svs/svs_shared.py
+++ src/ndn/svs/svs_shared.py
@@ -31,7 +31,8 @@ class SVSyncShared(SVSyncBase):
         while retries+1 > 0:
             try:
                 logging.info(f'SVSync: fetching data {Name.to_str(name)}')
-                ex_int_name, meta_info, content = await self.app.express_interest(name, must_be_fresh=True, can_be_prefix=False, lifetime=6000)
+                _, _, _, pkt = await self.app.express_interest(name, need_raw_packet=True, must_be_fresh=True, can_be_prefix=False, lifetime=6000)
+                ex_int_name, meta_info, content, sig_ptrs = parse_data(pkt)
                 logging.info(f'SVSync: received data {bytes(content)}')
                 if content and self.cacheOthers:
                     data_packet = make_data(name, MetaInfo(freshness_period=5000), content=bytes(content))
