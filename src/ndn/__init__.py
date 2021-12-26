6a9739b93a27c5afbd7fedc589d1bab64af0669c
Justin C Presley <justincpresley@gmail.com>
2021-12-26 04:53:17 -0600

    update leading comments

--- src/ndn/__init__.py
+++ src/ndn/__init__.py
@@ -2,7 +2,5 @@
 #    @Author-Email: justincpresley@gmail.com
 #    @Project: NDN State Vector Sync Protocol
 #    @Source-Code: https://github.com/justincpresley/ndn-python-svs
-#    @Pip-Library: https://pypi.org/project/ndn-svs/
-
-# Version of SVS according to pip
-__version__ = "0.2.2"
+#    @Pip-Library: https://pypi.org/project/ndn-svs
+#    @Documentation: https://ndn-python-svs.readthedocs.io
